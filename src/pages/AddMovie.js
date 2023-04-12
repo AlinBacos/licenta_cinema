64346b8db4472cf9496916d1f4464968e39b64e0
alinbacos1 <alinbacos@yahoo.com>
2023-04-12 22:48:04 +0300

    AddMovieForm

--- src/pages/AddMovie.js
+++ src/pages/AddMovie.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./pages_style/AddMovie.css"
+import AddMovieForm from '../components/AddMovieForm'
 
 function AddMovie() {
   return (
     <div className="add-movie">
-
+        <AddMovieForm/>
     </div>
   )
 }
